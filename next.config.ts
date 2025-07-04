7daa9a791d966f6ac744d5046baf6369bcfed846
Diop Makhtar <solidarcashnetwork@gmail.com>
2025-07-04 14:11:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'qozkatipbwbbqyjgxdmt.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
